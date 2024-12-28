2a7bf27c38f7ebedc66d5d357ef4fb58a1518aa0
qrqrq23r <qrqrq23r@gmail.com>
2024-12-28 16:49:26 +0900

    jpa test crud

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
@@ -17,10 +18,13 @@ import java.util.Set;
 @Getter
 @ToString
 @Table(indexes = {
-        @Index(columnList = "content"),
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
 
     @Id
@@ -31,6 +35,11 @@ public class Article {
     @Setter @Column(nullable = false, length = 10000) private String content; // 본문
 
     @Setter private String hashtag; // 해시태그
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
     @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
     @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
